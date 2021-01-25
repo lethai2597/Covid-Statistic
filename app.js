8431e48df00f5610b497e85b16df2f8786635b30
lethai2597 <lethai2597@gmail.com>
2021-01-25 10:36:10 +0700

    update search feature

--- app.js
+++ app.js
@@ -2,13 +2,7 @@ const covid = document.querySelector('#covid')
 
 const loading = `<div class="col-span-3 w-full flex items-center justify-center text-3xl"><img class="w-56" src="loading.gif" /></div>`
 
-let datas, defaultDatas = null
-
-fetch('https://api.covid19api.com/summary').then(response => response.json()).then(data => {
-    datas = data
-    defaultDatas = data
-    allCountries()
-})
+let global, countries, defaultCountries = null
 
 const render = (content) => {
     covid.innerHTML = `
@@ -25,8 +19,8 @@ const render = (content) => {
     `
 }
 
-const allCountries = (hasback = false) => {
-    let content = hasback ? `
+const allCountries = (hasBack = false) => {
+    let content = hasBack ? `
         <li class="rounded-lg col-span-3 font-semibold" onclick="back()">
             <button class="px-4 py-2 rounded border bg-white hover:bg-gray-200">⤺ Trở lại</buton>
         </li>
@@ -37,39 +31,39 @@ const allCountries = (hasback = false) => {
         <ul class="grid grid-cols-3 gap-4 col-span-3">
             <li class="p-6 rounded-lg shadow bg-white hover:bg-gray-50">
                 <div class="text-gray-600 text-sm font-semibold mb-6">Ca nhiễm mới</div>
-                <div class="text-3xl text-center text-gray-600 mb-10">${new Intl.NumberFormat().format(datas.Global.NewConfirmed)}</div>
+                <div class="text-3xl text-center text-gray-600 mb-10">${new Intl.NumberFormat().format(global.NewConfirmed)}</div>
             </li>
             <li class="p-6 rounded-lg shadow bg-white hover:bg-gray-50">
                 <div class="text-gray-600 text-sm font-semibold mb-6">Ca tử vong mới</div>
-                <div class="text-3xl text-center text-gray-600 mb-10">${new Intl.NumberFormat().format(datas.Global.NewDeaths)}</div>
+                <div class="text-3xl text-center text-gray-600 mb-10">${new Intl.NumberFormat().format(global.NewDeaths)}</div>
             </li>
             <li class="p-6 rounded-lg shadow bg-white hover:bg-gray-50">
                 <div class="text-gray-600 text-sm font-semibold mb-6">Ca phục hồi mới</div>
-                <div class="text-3xl text-center text-gray-600 mb-10">${new Intl.NumberFormat().format(datas.Global.NewRecovered)}</div>
+                <div class="text-3xl text-center text-gray-600 mb-10">${new Intl.NumberFormat().format(global.NewRecovered)}</div>
             </li>
             <li class="p-6 rounded-lg shadow bg-white hover:bg-gray-50">
                 <div class="text-gray-600 text-sm font-semibold mb-6">Tổng ca nhiễm</div>
-                <div class="text-3xl text-center text-gray-600 mb-10">${new Intl.NumberFormat().format(datas.Global.TotalConfirmed)}</div>
+                <div class="text-3xl text-center text-gray-600 mb-10">${new Intl.NumberFormat().format(global.TotalConfirmed)}</div>
             </li>
             <li class="p-6 rounded-lg shadow bg-white hover:bg-gray-50">
                 <div class="text-gray-600 text-sm font-semibold mb-6">Tổng ca tử vong</div>
-                <div class="text-3xl text-center text-gray-600 mb-10">${new Intl.NumberFormat().format(datas.Global.TotalDeaths)}</div>
+                <div class="text-3xl text-center text-gray-600 mb-10">${new Intl.NumberFormat().format(global.TotalDeaths)}</div>
             </li>
             <li class="p-6 rounded-lg shadow bg-white hover:bg-gray-50">
                 <div class="text-gray-600 text-sm font-semibold mb-6">Tổng ca phục hồi</div>
-                <div class="text-3xl text-center text-gray-600 mb-10">${new Intl.NumberFormat().format(datas.Global.TotalRecovered)}</div>
+                <div class="text-3xl text-center text-gray-600 mb-10">${new Intl.NumberFormat().format(global.TotalRecovered)}</div>
             </li>
         </ul>
         <h1 class="col-span-3 text-2xl text-gray-700">
-            Theo khu vực
+            Theo quốc gia
         </h1>
     `
-    if(datas.Countries.length > 0){
-        for(let i = 0; i < datas.Countries.length; i++){
+    if(countries.length > 0){
+        for(let i = 0; i < countries.length; i++){
             content += `
                 <li class="p-6 rounded-lg shadow bg-white cursor-pointer hover:bg-gray-50" onclick="byCountry(${i})">
-                    <div class="text-gray-600 text-sm font-semibold mb-6">${datas.Countries[i].Country}</div>
-                    <div class="text-3xl text-center text-gray-600 mb-10">${new Intl.NumberFormat().format(datas.Countries[i].TotalConfirmed)}</div>
+                    <div class="text-gray-600 text-sm font-semibold mb-6">${countries[i].Country}</div>
+                    <div class="text-3xl text-center text-gray-600 mb-10">${new Intl.NumberFormat().format(countries[i].TotalConfirmed)}</div>
                 </li>
             `
         }
@@ -86,44 +80,45 @@ const byCountry = (i) => {
             <button class="px-4 py-2 rounded border bg-white hover:bg-gray-200">⤺ Trở lại</buton>
         </li>
         <li class="rounded-lg col-span-3 font-semibold px-1 py-3">
-            <h2 class="text-3xl text-gray-700">${datas.Countries[i].Country}</h2>
-            <h3 class="text-sm">#${datas.Countries[i].CountryCode}</h3>
+            <h2 class="text-3xl text-gray-700">${countries[i].Country}</h2>
+            <h3 class="text-sm">#${countries[i].CountryCode}</h3>
         </li>
         <li class="p-6 rounded-lg shadow bg-white hover:bg-gray-50">
             <div class="text-gray-600 text-sm font-semibold mb-6">Ca nhiễm mới</div>
-            <div class="text-3xl text-center text-gray-600 mb-10">${datas.Countries[i].NewConfirmed}</div>
+            <div class="text-3xl text-center text-gray-600 mb-10">${countries[i].NewConfirmed}</div>
         </li>
         <li class="p-6 rounded-lg shadow bg-white hover:bg-gray-50">
             <div class="text-gray-600 text-sm font-semibold mb-6">Ca tử vong mới</div>
-            <div class="text-3xl text-center text-gray-600 mb-10">${datas.Countries[i].NewDeaths}</div>
+            <div class="text-3xl text-center text-gray-600 mb-10">${countries[i].NewDeaths}</div>
         </li>
         <li class="p-6 rounded-lg shadow bg-white hover:bg-gray-50">
             <div class="text-gray-600 text-sm font-semibold mb-6">Ca phục hồi mới</div>
-            <div class="text-3xl text-center text-gray-600 mb-10">${datas.Countries[i].NewRecovered}</div>
+            <div class="text-3xl text-center text-gray-600 mb-10">${countries[i].NewRecovered}</div>
         </li>
         <li class="p-6 rounded-lg shadow bg-white hover:bg-gray-50">
             <div class="text-gray-600 text-sm font-semibold mb-6">Tổng ca nhiễm</div>
-            <div class="text-3xl text-center text-gray-600 mb-10">${datas.Countries[i].TotalConfirmed}</div>
+            <div class="text-3xl text-center text-gray-600 mb-10">${countries[i].TotalConfirmed}</div>
         </li>
         <li class="p-6 rounded-lg shadow bg-white hover:bg-gray-50">
             <div class="text-gray-600 text-sm font-semibold mb-6">Tổng ca tử vong</div>
-            <div class="text-3xl text-center text-gray-600 mb-10">${datas.Countries[i].TotalDeaths}</div>
+            <div class="text-3xl text-center text-gray-600 mb-10">${countries[i].TotalDeaths}</div>
         </li>
         <li class="p-6 rounded-lg shadow bg-white hover:bg-gray-50">
             <div class="text-gray-600 text-sm font-semibold mb-6">Tổng ca phục hồi</div>
-            <div class="text-3xl text-center text-gray-600 mb-10">${datas.Countries[i].TotalRecovered}</div>
+            <div class="text-3xl text-center text-gray-600 mb-10">${countries[i].TotalRecovered}</div>
         </li>
     `
     render(content)
 }
 
 const back = () => {
-    datas = defaultDatas
+    countries = defaultCountries
     allCountries()
 }
 const search = () => {
     let text = document.querySelector('#search').value.toLowerCase()
-    datas.Countries = defaultDatas.Countries.filter(item => JSON.stringify(item).toLowerCase().includes(removeAccents(text)))
+    countries = defaultCountries.filter(item => JSON.stringify(item).toLowerCase().includes(removeAccents(text)))
+    console.log(defaultCountries)
     allCountries(true)
 }
 
@@ -151,4 +146,11 @@ function removeAccents(str) {
     return str;
 }
 
+
+fetch('https://api.covid19api.com/summary').then(response => response.json()).then(data => {
+    global = data.Global
+    countries = data.Countries
+    defaultCountries = data.Countries
+    allCountries()
+})
 render(loading)
